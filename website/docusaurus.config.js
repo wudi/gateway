b4adb9daba969b90c11a35ab2576ff71871f969a
Di Wu <hi@wudi.io>
2026-02-27 10:59:02 +0800

    Enabling Search

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -26,7 +26,12 @@ const config = {
       onBrokenMarkdownLinks: 'throw',
     },
   },
-
+  algolia: {
+    apiKey: 'a01d7658dd31235198a49ca526f74a37',
+    indexName: 'Runway API Gateway',
+    appId: 'VK6HD2PAZ5',
+    algoliaOptions: {}
+  },
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
